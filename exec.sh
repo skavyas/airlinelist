e94dbc0ae0e79a0ca770ae6bf47ee25eb2cd0d1b
skavyas <kavya.noni@gmail.com>
2018-01-26 14:44:09 -0800

    Airline

---
+++ exec.sh
@@ -0,0 +1,6 @@
+echo "# airlinelist" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/skavyas/airlinelist.git
+git push -u origin master
